b332125dfbbcf90d6a15522b8d0a52a5aaf8c9c1
glucan <gluelaver@live.jp>
2019-11-01 16:52:29 +0900

    db test

--- Manager.php
+++ Manager.php
@@ -3,9 +3,9 @@ function connect()
 {
     $env = getenv('DATABASE_URL');
     echo $env;
-    $dsn = 'pgsql:host=; dbname=; sslmode=;';
-    $usr = '';
-    $passwd = '';
+    $dsn = 'pgsql:host='.getenv('DATABASE_HOST').'; dbname='.getenv('DATABASE_NAME').';';
+    $usr = getenv('DATABASE_USER');
+    $passwd = getenv('DATABASE_PASSWORD');
 
     try {
         $db = new PDO($dsn, $usr, $passwd);
